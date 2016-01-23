87cea54adaae0289c540e146a59f0a0cc60845ae
turingware <turingware@users.noreply.github.com>
2016-01-23 15:20:51 -0600

    Create mod.cpp

---
+++ 1-4_ammoBoxes/mod.cpp
@@ -0,0 +1,10 @@
+//Version 23.01.16
+//23.01.16 Update: Fixed picture and overviewPicture properties to point to the correct location of logo256.paa
+
+name = "1st Battalion, 4th Marines Ammoboxes";
+picture = "\loadoutmanager\logo256.paa";
+description = "1-4th Ammoboxes - Version 1.0.0";
+tooltip = "1-4th";
+overview = "1st Battalion, 4th Marines ammoboxes, built on Riouken's framework.";
+author = "Cpl D. Warren";
+overviewPicture = "\loadoutmanager\logo256.paa";
